e1d1d0cc5e728e3b51b1193396b582daf69cd969
Elias Vedovato <eliasvedovato@gmail.com>
2022-11-10 00:23:11 -0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import NikeStore from '../assets/projects/nike-store.png'
+import WatchesEcomm from '../assets/projects/watches-website.png'
+import LandingPage from '../assets/projects/landing-page.png'
+import RickAndMortyAPI from '../assets/projects/rick-and-morty-API.png'
+
+export const data = [
+	{
+		id: 1,
+		name: 'React JS Application',
+		image: NikeStore,
+		github: 'https://github.com/eliasvedovato/nike_ecommerce',
+		live: 'https://nike-ecommerce-psi.vercel.app/',
+	},
+	{
+		id: 2,
+		name: 'JS Application',
+		image: WatchesEcomm,
+		github: 'https://github.com/eliasvedovato/rick_and_morty_api',
+		live: 'https://eliasvedovato.github.io/rick_and_morty_api/',
+	},
+	{
+		id: 3,
+		name: 'React JS Application',
+		image: LandingPage,
+		github: 'https://github.com/eliasvedovato/tailwind-landing-page',
+		live: 'https://tailwind-landing-page-xi-one.vercel.app/',
+	},
+	{
+		id: 4,
+		name: 'React JS Application',
+		image: RickAndMortyAPI,
+		github: 'https://github.com/eliasvedovato/watches-ecommerce',
+		live: 'https://watches-ecommerce.vercel.app',
+	},
+]
