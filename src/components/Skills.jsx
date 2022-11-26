eb1695b01144def2f051afa18d2715e2b1af2a61
Elias Vedovato <eliasvedovato@gmail.com>
2022-11-26 18:19:10 -0300

    add next to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import CSS from '../assets/css.png'
 import JavaScript from '../assets/javascript.png'
 import ReactImg from '../assets/react.png'
 import Node from '../assets/node.png'
-import FireBase from '../assets/firebase.png'
+import Next from '../assets/next.png'
 import GitHub from '../assets/github.png'
 import Tailwind from '../assets/tailwind.png'
 
@@ -86,10 +86,10 @@ const Skills = () => {
 					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
 						<img
 							className='w-20 mx-auto'
-							src={FireBase}
+							src={Next}
 							alt='HTML icon'
 						/>
-						<p className='my-4'>FIREBASE</p>
+						<p className='my-4'>NEXT JS</p>
 					</div>
 				</div>
 			</div>
